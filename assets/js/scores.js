248db6db94aa6b080ab75b98fe7fd9392c2d2f59
shingchan95 <chanhei888@gmail.com>
2022-04-08 20:27:05 +0100

    update

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -12,7 +12,7 @@ function getInfo(){
         addLi=highScores.appendChild(li1);
         //li1.textcontent= InL + " score is: "+scR;
         
-        li1.textcontent= 
+
         li1.append(localStorage.getItem((localStorage.key(i))));
         console.log(localStorage.getItem((localStorage.key(i))));
         
@@ -30,9 +30,9 @@ function getInfo(){
          submitButton.addEventListener("click", function(){
              var initial=document.getElementById("initials").value;
              window.location = "assets/highscores.html"
-             window.localStorage.setItem("initial",JSON.stringify(initial));
+             window.localStorage.setItem("key",JSON.stringify(initial, score));
            
-             window.localStorage.setItem("score",JSON.stringify(score));
+
              
          });
      }
